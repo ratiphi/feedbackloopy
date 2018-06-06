33f7fb2dbf1017682656b0ee31d21e8be596816c
Tim Turner <timmyturner@me.com>
2018-06-05 21:17:18 -0700

    google OAuth working

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
 'use strict';
 
-
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const keys = require('../config/keys');
 
 const app = express();
 
@@ -13,10 +12,19 @@ passport.use(
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-  }, (accessToken) => {
-    console.log(accessToken);
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('access token: ', accessToken);
+    console.log('refresh token: ', refreshToken);
+    console.log('profile: ', profile);
+    console.log('done: ', done);
   })
 );
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
